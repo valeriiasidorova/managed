7adedf5bc5b7125f74656b91a210a19885cb3a21
Valeriia Sidorova <valeriia.sidorova@gmail.com>
2024-11-29 21:54:59 +0100

    add Board tasks

--- client/src/app/projects/BoardView/index.tsx
+++ client/src/app/projects/BoardView/index.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 import { useGetTasksQuery } from "@/state/api";
 import { Task as TaskType } from "@/state/api";
-import { EllipsisVertical, Plus } from "lucide-react";
+import { EllipsisVertical, MessageSquareMore, Plus } from "lucide-react";
+import Image from "next/image";
+import { format } from "date-fns";
 
 type BoardProps = {
   id: string;
@@ -37,7 +39,7 @@ type TaskColumnProps = {
 
 const TaskColumn = ({ status, tasks }: TaskColumnProps) => {
   const tasksCount = tasks.filter((task) => task.status === status).length;
-  
+
   const statusColor: any = {
     "To Do": "#2563EB",
     "Work In Progress": "#059669",
@@ -71,6 +73,145 @@ const TaskColumn = ({ status, tasks }: TaskColumnProps) => {
             <button className="flex h-6 w-6 items-center justify-center rounded bg-gray-200 dark:bg-dark-tertiary dark:text-white">
               <Plus size={16} />
             </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Display tasks */}
+      {tasks
+        .filter((task) => task.status === status)
+        .map((task) => (
+          <Task key={task.id} task={task} />
+        ))}
+    </div>
+  );
+};
+
+/* Task */
+type TaskProps = {
+  task: TaskType;
+};
+
+const Task = ({ task }: TaskProps) => {
+  const taskTagsSplit = task.tags ? task.tags.split(",") : [];
+
+  const formattedStartDate = task.startDate
+    ? format(new Date(task.startDate), "dd/MM/yyyy")
+    : "";
+  const formattedDueDate = task.dueDate
+    ? format(new Date(task.dueDate), "dd/MM/yyyy")
+    : "";
+
+  const numberOfComments = (task.comments && task.comments.length) || 0;
+
+  const PriorityTag = ({ priority }: { priority: TaskType["priority"] }) => (
+    <div
+      className={`rounded-full px-2 py-1 text-xs font-semibold ${
+        priority === "Urgent"
+          ? "bg-red-200 text-red-700"
+          : priority === "High"
+            ? "bg-yellow-200 text-yellow-700"
+            : priority === "Medium"
+              ? "bg-green-200 text-green-700"
+              : priority === "Low"
+                ? "bg-blue-200 text-blue-700"
+                : "bg-gray-200 text-gray-700"
+      }`}
+    >
+      {priority}
+    </div>
+  );
+
+  return (
+    <div className="mb-4 rounded-md bg-white shadow dark:bg-dark-secondary">
+      {/* Attachments */}
+      {task.attachments && task.attachments.length > 0 && (
+        <Image
+          src={`/${task.attachments[0].fileURL}`}
+          alt={task.attachments[0].fileName}
+          width={400}
+          height={200}
+          className="h-auto w-full rounded-t-md"
+        />
+      )}
+
+      <div className="p-4 md:p-6">
+        <div className="flex items-start justify-between">
+          {/* Tags */}
+          <div className="flex flex-1 flex-wrap items-center gap-2">
+            {task.priority && <PriorityTag priority={task.priority} />}
+            <div className="flex gap-2">
+              {taskTagsSplit.map((tag) => (
+                <div
+                  key={tag}
+                  className="rounded-full bg-blue-100 px-2 py-1 text-xs"
+                >
+                  {tag}
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <button className="flex h-6 w-4 flex-shrink-0 items-center justify-center dark:text-neutral-500">
+            <EllipsisVertical size={26} />
+          </button>
+        </div>
+
+        {/* Task title and points */}
+        <div className="my-3 flex justify-between">
+          <h4 className="text-md font-bold dark:text-white">{task.title}</h4>
+
+          {typeof task.points === "number" && (
+            <div className="text-xs font-semibold dark:text-white">
+              {task.points} pts
+            </div>
+          )}
+        </div>
+
+        {/* Formatted dates */}
+        <div className="text-xs text-gray-500 dark:text-neutral-500">
+          {formattedStartDate && <span>{formattedStartDate} - </span>}
+          {formattedDueDate && <span>{formattedDueDate} </span>}
+        </div>
+
+        {/* Task description */}
+        <p className="pt-1 text-sm text-gray-600 dark:text-neutral-500">
+          {task.description}
+        </p>
+
+        {/* Horizontal line */}
+        <div className="mt-4 border-t border-gray-200 dark:border-stroke-dark" />
+
+        <div className="mt-3 flex items-center justify-between">
+          {/* Users */}
+          <div className="flex -space-x-[6px] overflow-hidden">
+            {task.assignee && (
+              <Image
+                key={task.assignee.userId}
+                src={`/${task.assignee.profilePictureUrl!}`}
+                alt={task.assignee.username}
+                width={30}
+                height={30}
+                className="h-8 w-8 rounded-full border-2 border-white object-cover dark:border-dark-secondary"
+              />
+            )}
+            {task.author && (
+              <Image
+                key={task.author.userId}
+                src={`/${task.author.profilePictureUrl!}`}
+                alt={task.author.username}
+                width={30}
+                height={30}
+                className="h-8 w-8 rounded-full border-2 border-white object-cover dark:border-dark-secondary"
+              />
+            )}
+          </div>
+          {/* Number of comments */}
+          <div className="flex items-center text-gray-500 dark:text-neutral-400">
+            <MessageSquareMore size={20} />
+            <span className="ml-1 text-sm dark:text-neutral-400">
+              {numberOfComments}
+            </span>
           </div>
         </div>
       </div>
