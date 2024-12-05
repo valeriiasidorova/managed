9b47415cf1b970dbc34dd3e866702bc1a2aeb9f4
Valeriia Sidorova <valeriia.sidorova@gmail.com>
2024-12-05 19:37:34 +0100

    create Tasks Table view

---
+++ client/src/app/projects/TableView/index.tsx
@@ -0,0 +1,94 @@
+import Header from "@/app/components/Header";
+import { useGetTasksQuery } from "@/state/api";
+import React from "react";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { dataGridClassNames, dataGridSxStyles } from "@/lib/utils";
+import { useAppSelector } from "@/app/redux";
+
+type TableViewProps = {
+  id: string;
+};
+
+const columns: GridColDef[] = [
+  {
+    field: "title",
+    headerName: "Title",
+    width: 100,
+  },
+  {
+    field: "description",
+    headerName: "Description",
+    width: 200,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 130,
+    renderCell: (params) => (
+      <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+        {params.value}
+      </span>
+    ),
+  },
+  {
+    field: "priority",
+    headerName: "Priority",
+    width: 75,
+  },
+  {
+    field: "tags",
+    headerName: "Tags",
+    width: 130,
+  },
+  {
+    field: "startDate",
+    headerName: "Start Date",
+    width: 130,
+  },
+  {
+    field: "dueDate",
+    headerName: "Due Date",
+    width: 130,
+  },
+  {
+    field: "author",
+    headerName: "Author",
+    width: 150,
+    renderCell: (params) => params.value?.username || "Unknown",
+  },
+  {
+    field: "assignee",
+    headerName: "Assignee",
+    width: 150,
+    renderCell: (params) => params.value?.username || "Unassigned",
+  },
+];
+
+const TableView = ({ id }: TableViewProps) => {
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+  const {
+    data: tasks,
+    isLoading,
+    error,
+  } = useGetTasksQuery({ projectId: Number(id) });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>An error occured while fetching tasks</div>;
+
+  return (
+    <div className="h-[540px] w-full px-4 pb-8 xl:px-6">
+      <div className="pt-5">
+        <Header name="Table" isSmallText />
+        {/* TODO: button */}
+      </div>
+      <DataGrid
+        rows={tasks || []}
+        columns={columns}
+        className={dataGridClassNames}
+        sx={dataGridSxStyles(isDarkMode)}
+      />
+    </div>
+  );
+};
+
+export default TableView;
--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -5,6 +5,7 @@ import ProjectHeader from "../ProjectHeader";
 import Board from "../BoardView";
 import List from "../ListView";
 import Timeline from "../TimelineView";
+import Table from "../TableView";
 
 type Props = {
   params: { id: string };
@@ -21,6 +22,7 @@ const Project = ({ params }: Props) => {
       {activeTab === "Board" && <Board id={id} />}
       {activeTab === "List" && <List id={id} />}
       {activeTab === "Timeline" && <Timeline id={id} />}
+      {activeTab === "Table" && <Table id={id} />}
     </div>
   );
 };
