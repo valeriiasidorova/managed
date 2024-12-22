9edba2dae8abd5860b836f3f9338968ce7b7085c
Valeriia Sidorova <valeriia.sidorova@gmail.com>
2024-12-22 17:39:51 +0100

    style: add z-index to Modal

--- client/src/app/components/Modal/index.tsx
+++ client/src/app/components/Modal/index.tsx
@@ -13,7 +13,7 @@ type ModalProps = {
 const Modal = ({ children, isOpen, onClose, name }: ModalProps) => {
   if (!isOpen) return null;
   return ReactDOM.createPortal(
-    <div className="fixed inset-0 flex h-full w-full items-center justify-center overflow-y-auto bg-gray-600 bg-opacity-50 p-4">
+    <div className="fixed inset-0 z-10 flex h-full w-full items-center justify-center overflow-y-auto bg-gray-600 bg-opacity-50 p-4">
       <div className="w-full max-w-2xl rounded-lg bg-white p-4 shadow-lg dark:bg-dark-secondary">
         <Header
           name={name}
