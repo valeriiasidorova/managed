e04bf97ef55b8ec97bf00462ba98f2608c9e075f
Valeriia Sidorova <valeriia.sidorova@gmail.com>
2025-04-18 18:18:16 +0200

    fix: update id sequences so psql autoincrement always starts at max

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from '@prisma/client';
-import fs from 'fs';
-import path from 'path';
+import { PrismaClient } from "@prisma/client";
+import fs from "fs";
+import path from "path";
 const prisma = new PrismaClient();
 
 async function deleteAllData(orderedFileNames: string[]) {
@@ -16,25 +16,50 @@ async function deleteAllData(orderedFileNames: string[]) {
   }
 }
 
+// Fix for unique constraint error (id) 
+// Updates sequences to the max ID
+// This way PostgreSQL's autoincrement starts at the correct (max) value
+async function syncSequences() {
+  const sequences = [
+    { table: "Team", column: "id" },
+    { table: "Project", column: "id" },
+    { table: "ProjectTeam", column: "id" },
+    { table: "User", column: "userId" },
+    { table: "Task", column: "id" },
+    { table: "Attachment", column: "id" },
+    { table: "Comment", column: "id" },
+    { table: "TaskAssignment", column: "id" },
+  ];
+
+  for (const { table, column } of sequences) {
+    const sequenceName = `"${table}_${column}_seq"`;
+    const sql = `
+      SELECT setval('${sequenceName}', COALESCE((SELECT MAX("${column}") FROM "${table}"), 1));
+    `;
+    await prisma.$executeRawUnsafe(sql);
+    console.log(`Sequence synced for ${table}`);
+  }
+}
+
 async function main() {
-  const dataDirectory = path.join(__dirname, 'seedData');
+  const dataDirectory = path.join(__dirname, "seedData");
 
   const orderedFileNames = [
-    'team.json',
-    'project.json',
-    'projectTeam.json',
-    'user.json',
-    'task.json',
-    'attachment.json',
-    'comment.json',
-    'taskAssignment.json',
+    "team.json",
+    "project.json",
+    "projectTeam.json",
+    "user.json",
+    "task.json",
+    "attachment.json",
+    "comment.json",
+    "taskAssignment.json",
   ];
 
   await deleteAllData(orderedFileNames);
 
   for (const fileName of orderedFileNames) {
     const filePath = path.join(dataDirectory, fileName);
-    const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    const jsonData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
     const modelName = path.basename(fileName, path.extname(fileName));
     const model: any = prisma[modelName as keyof typeof prisma];
 
@@ -44,6 +69,8 @@ async function main() {
 
     console.log(`Seeded ${modelName} with data from ${fileName}`);
   }
+
+  await syncSequences();
 }
 
 main()
